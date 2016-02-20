e4875806679f9d81d23aee7f6ddf90a5b927e70c
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-20 14:44:06 -0700

    add song model and feature test, as well as appropriate relationship with artist

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -17,12 +17,3 @@ RSpec.feature "User submits a new song" do
     expect(page).to have_link artist.name, href: artist_path(artist)
   end
 end
-
-As a user
-Given that artists exist in the database
-When I visit the artist songs index
-And I click "New song"
-And I fill in the title
-And I click "Create Song"
-Then I should see the song name
-And I should see a link to the song artist's individual page
