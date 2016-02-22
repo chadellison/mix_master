bc23491b54d7ed784b9d49fa6ff3824aee8cba8f
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-21 17:47:42 -0700

    refactor forms and error messages through partials

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
   def index
     @artists = Artist.all
   end
@@ -8,7 +10,6 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def create
@@ -21,21 +22,26 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-    @artist.update(artist_params)
+    if @artist.update(artist_params)
     redirect_to @artist
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @artist = Artist.find(params[:id]).destroy
+    Artist.find(params[:id]).destroy
     redirect_to artists_path
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name, :image_path)
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update]
   def artist_index
     @artist = Artist.find(params[:artist_id])
     @songs = @artist.songs.order(:title)
@@ -24,15 +25,12 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
     @song.update(song_params)
 
     redirect_to @song
@@ -45,6 +43,10 @@ class SongsController < ApplicationController
   end
 
   private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
